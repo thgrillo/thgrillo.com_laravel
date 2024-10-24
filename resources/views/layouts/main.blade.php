fb4601ef7e4161abf6c2db05ff4b290b20f3f1d5
Thiago Grillo <thiago_grillo@hotmail.com>
2024-10-24 13:50:15 -0300

    gtag colocado na main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -34,7 +34,14 @@
         <link href="https://fonts.googleapis.com/css2?family=Nabla&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400&amp;display=swap" rel="stylesheet">
 
-
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-98M702D332"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+      
+          gtag('config', 'G-98M702D332');
+        </script>
         <!-- ===============================================-->
         <!--    Stylesheets-->
         <!-- ===============================================-->
